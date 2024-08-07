f53456791d4007ec48025b5a51e8d23f2295b600
Joseph Lu <jlu@c2r5p12.hive.fi>
2024-08-07 18:33:37 +0300

    a little bit of cpp00ex01

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlu <jlu@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/07 17:33:45 by jlu               #+#    #+#             */
-/*   Updated: 2024/08/07 18:33:01 by jlu              ###   ########.fr       */
+/*   Created: 2024/08/07 17:33:50 by jlu               #+#    #+#             */
+/*   Updated: 2024/08/07 17:33:51 by jlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook.hpp"
-#include <iostream>
-
-int main()
-{
-  std::string inputName;
-
-  std::cout << "Enter your information:" << std::endl;
-  std::cout << "Name: ";
-  std::getline(std::cin, inputName);
-  std::cout << inputName << std::endl;
-  // PhoneBook::getName(std::cin);
-
-  return 0;  
-};
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlu <jlu@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/07 17:33:45 by jlu               #+#    #+#             */
-/*   Updated: 2024/08/07 18:33:01 by jlu              ###   ########.fr       */
+/*   Created: 2024/08/07 17:33:55 by jlu               #+#    #+#             */
+/*   Updated: 2024/08/07 18:18:39 by jlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook.hpp"
-#include <iostream>
-
-int main()
+class PhoneBook
 {
-  std::string inputName;
+    char *name;
+    char *PhoneNumber;
 
-  std::cout << "Enter your information:" << std::endl;
-  std::cout << "Name: ";
-  std::getline(std::cin, inputName);
-  std::cout << inputName << std::endl;
-  // PhoneBook::getName(std::cin);
-
-  return 0;  
+    char *getName(char *str);
+    char *getNumber(char *str);
 };
