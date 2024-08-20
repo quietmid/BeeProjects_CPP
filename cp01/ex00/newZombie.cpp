0fa7297adf9f359411f630197999d25aefa5fd7c
quietmid <ninjonglu@gmail.com>
2024-08-20 23:52:53 +0300

    rm folder

--- cp01/ex00/newZombie.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "Zombie.hpp"
-
-
-/* the new operator returns a pointer to the newly created `Zombie` object on the heap*/
-Zombie* newZombie(std::string name)
-{
-	return (new Zombie(name));
-};
