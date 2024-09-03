dedaee129be8d2ceb16b84ce998c19b8a968d716
quietmid <ninjonglu@gmail.com>
2024-09-03 15:52:16 +0300

    ex00 updated

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,6 +6,8 @@
 int main()
 {
 	const Animal* meta = new Animal();
+	std::string type = "sparrow";
+	const Animal* m2 = new Animal(type);
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 	const WrongAnimal* wrong = new WrongAnimal();
@@ -14,16 +16,19 @@ int main()
 	std::cout << "j is " << j->getType() << " " << std::endl;
 	std::cout << "i is "<< i->getType() << " " << std::endl;
 	std::cout << "meta is "<< meta->getType() << " " << std::endl;
+	std::cout << "m2 is "<< m2->getType() << " " << std::endl;
 	std::cout << "wrong is "<< wrong->getType() << " " << std::endl;
 	std::cout << "wrg is "<< wrg->getType() << " " << std::endl;
 	std::cout << "-------------------" << std::endl;
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
+	m2->makeSound();
 	wrong->makeSound();
 	wrg->makeSound();
 	std::cout << "-------------------" << std::endl;
 	delete meta;
+	delete m2;
 	delete j;
 	delete i;
 	delete wrong;
